fee8829c9c03e90a7dbe44987047f4a07a031f4f
hhhaan <hhanheon@gmail.com>
2025-05-31 18:55:27 +0900

    dev용 설정 추가

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,9 @@ import { type NextRequest } from 'next/server';
 import { updateSession } from '@/src/shared/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
+    // if (process.env.NODE_ENV === 'development') {
+    //     return;
+    // }
     return await updateSession(request);
 }
 
