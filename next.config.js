6e4128289b452d2d6a643aa7ef0e5606181a225d
hhhaan <hhanheon@gmail.com>
2025-06-04 21:04:36 +0900

    refactor: goodbye graphql

--- next.config.js
+++ next.config.js
@@ -3,13 +3,5 @@ const nextConfig = {
     images: {
         domains: ['k.kakaocdn.net'],
     },
-    async rewrites() {
-        return [
-            {
-                source: '/graphql',
-                destination: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/graphql`,
-            },
-        ];
-    },
 };
 module.exports = nextConfig;
