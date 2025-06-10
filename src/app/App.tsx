e2a5b98672208c16e9d62c6e20dded7fd3daa73d
hhhaan <hhanheon@gmail.com>
2025-06-11 02:53:26 +0900

    feat: react toast 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Provider from '@/src/app/providers/provider';
 import './styles/globals.css';
+import { Toaster } from 'react-hot-toast';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -26,7 +27,10 @@ export default function App({
     return (
         <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <Provider>{children}</Provider>
+                <Provider>
+                    <Toaster position="bottom-center" />
+                    {children}
+                </Provider>
             </body>
         </html>
     );
